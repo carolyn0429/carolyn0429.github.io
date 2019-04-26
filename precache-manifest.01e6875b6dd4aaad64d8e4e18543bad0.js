afc9e95f07d1b2afc8d522a210fec69148be255b
Carolyn Hung <carolyn.hung@n26.com>
2019-04-26 17:44:33 +0200

    Updates

---
+++ precache-manifest.01e6875b6dd4aaad64d8e4e18543bad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b8206109908e189371bee3b3c88f8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2af6c80fbcedfe0a2869",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "b2838d3c9e4cf4694ddf",
+    "url": "/static/js/2.ab533eb6.chunk.js"
+  },
+  {
+    "revision": "2af6c80fbcedfe0a2869",
+    "url": "/static/js/main.5e54497a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
